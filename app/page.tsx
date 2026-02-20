678eee52a4d78c52546789a075649af58a1925b6
Charles gravina <charlesagravina@gmail.com>
2026-02-20 13:18:12 -0500

    Update homepage content

--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,10 @@
-import Image from "next/image";
-import Link from "next/link";
-
-import styles from "./page.module.css";
-
-const tiles = [
-  {
-    label: "Bridge Builder",
-    href: "/tools/bridge",
-    src: "/ui/bridge-button.png",
-  },
-];
+import HubLayout from "./(hub)/layout";
+import HubHome from "./(hub)/page";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <div
-        style={{ display: "flex", justifyContent: "center", alignItems: "center" }}
-      >
-        {tiles.map((tile) => (
-          <Link key={tile.label} href={tile.href} className={styles.tile}>
-            <Image
-              src={tile.src}
-              alt={tile.label}
-              width={420}
-              height={140}
-              className={styles.image}
-              priority
-              unoptimized
-            />
-          </Link>
-        ))}
-      </div>
-    </div>
+    <HubLayout>
+      <HubHome />
+    </HubLayout>
   );
 }
